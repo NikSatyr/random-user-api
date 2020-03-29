f9e0747d63f2f3f5ccaff54e1550d0a4bb843973
Nik Satyr <>
2020-03-29 16:17:05 +0300

    fixed return type

--- app/src/main/java/com/niksatyr/randomuser/api/RandomUserApi.kt
+++ app/src/main/java/com/niksatyr/randomuser/api/RandomUserApi.kt
@@ -1,11 +1,13 @@
 package com.niksatyr.randomuser.api
 
+import com.niksatyr.randomuser.dto.User
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface RandomUserApi {
 
     @GET("/")
-    fun getUsers(@Query("results") count: Int)
+    fun getUsers(@Query("results") count: Int): Response<List<User>>
 
 }
