62ff000ba6e0c41d6106479c6888b2a5ef2737d9
Nik Satyr <>
2020-03-30 23:47:21 +0300

    support for night mode

--- app/src/main/java/com/niksatyr/randomuser/util/App.kt
+++ app/src/main/java/com/niksatyr/randomuser/util/App.kt
@@ -1,4 +1,17 @@
 package com.niksatyr.randomuser.util
 
-class App {
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import com.niksatyr.randomuser.repo.LocalSettingsRepository
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val settingsRepository = LocalSettingsRepository(this)
+        AppCompatDelegate.setDefaultNightMode(when (settingsRepository.isNightModeEnabled()) {
+            true -> AppCompatDelegate.MODE_NIGHT_YES
+            false -> AppCompatDelegate.MODE_NIGHT_NO
+        })
+    }
 }
