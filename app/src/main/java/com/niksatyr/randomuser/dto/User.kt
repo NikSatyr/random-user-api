1436abaa9faa41d7a6412c83e655945f07c7d530
Nik Satyr <>
2020-03-30 21:40:14 +0300

    made user parcelable

--- app/src/main/java/com/niksatyr/randomuser/dto/User.kt
+++ app/src/main/java/com/niksatyr/randomuser/dto/User.kt
@@ -1,30 +1,36 @@
 package com.niksatyr.randomuser.dto
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 import java.util.*
 
+@Parcelize
 data class User(
     val name: Name,
     @SerializedName("dob") val dateOfBirth: DateOfBirth,
     @SerializedName("picture") val photoUrls: PhotoUrls,
     val email: String,
     val phone: String
-) {
+) : Parcelable {
 
+    @Parcelize
     data class Name(
         val title: String,
         val first: String,
         val last: String
-    )
+    ) : Parcelable
 
+    @Parcelize
     data class DateOfBirth(
         val date: Date,
         val age: Int
-    )
+    ) : Parcelable
 
+    @Parcelize
     data class PhotoUrls(
         val large: String,
         val medium: String,
         val thumbnail: String
-    )
+    ) : Parcelable
 }
