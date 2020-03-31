13bd4401abc2248db34a1d6f0a03dc60010e97b9
Nik Satyr <>
2020-03-31 12:40:06 +0300

    night mode changes are now applied instantly

--- app/src/main/java/com/niksatyr/randomuser/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/com/niksatyr/randomuser/ui/fragment/SettingsFragment.kt
@@ -3,15 +3,28 @@ package com.niksatyr.randomuser.ui.fragment
 import android.os.Bundle
 import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import com.niksatyr.randomuser.R
+import com.niksatyr.randomuser.util.App
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.settings)
-        val usersCountPreference: EditTextPreference = findPreference("pref_users_count") ?:
-                throw IllegalStateException("Failed to find users count preference")
-        // TODO check bounds
+        val usersCountPreference: EditTextPreference = findPreference("pref_users_count")
+            ?: throw IllegalStateException("Failed to find users count preference")
+        val nightModePreference: SwitchPreference = findPreference("pref_night_mode")
+            ?: throw IllegalStateException("Failed to find night mode preference")
+
+        nightModePreference.setOnPreferenceChangeListener { _, newValue ->
+            handleNightModeChange(newValue as Boolean)
+            true
+        }
+    }
+
+    private fun handleNightModeChange(nightModeEnabled: Boolean) {
+        val app = activity?.application as App
+        app.setNightMode(nightModeEnabled)
     }
 
 }
--- app/src/main/java/com/niksatyr/randomuser/util/App.kt
+++ app/src/main/java/com/niksatyr/randomuser/util/App.kt
@@ -9,7 +9,11 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         val settingsRepository = LocalSettingsRepository(this)
-        AppCompatDelegate.setDefaultNightMode(when (settingsRepository.isNightModeEnabled()) {
+        setNightMode(settingsRepository.isNightModeEnabled())
+    }
+
+    fun setNightMode(nightModeEnabled: Boolean) {
+        AppCompatDelegate.setDefaultNightMode(when (nightModeEnabled) {
             true -> AppCompatDelegate.MODE_NIGHT_YES
             false -> AppCompatDelegate.MODE_NIGHT_NO
         })
