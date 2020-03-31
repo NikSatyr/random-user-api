f866b3d82759b94809475375688311d4d637c021
Nik Satyr <>
2020-03-31 12:49:21 +0300

    back navigation from settings

--- app/src/main/java/com/niksatyr/randomuser/ui/activity/SettingsActivity.kt
+++ app/src/main/java/com/niksatyr/randomuser/ui/activity/SettingsActivity.kt
@@ -1,6 +1,27 @@
 package com.niksatyr.randomuser.ui.activity
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.niksatyr.randomuser.R
 
-class SettingsActivity : AppCompatActivity(R.layout.activity_settings)
+class SettingsActivity : AppCompatActivity(R.layout.activity_settings) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish() // We won't need this activity, hence may be killed
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish() // We won't need this activity, hence may be killed
+        return super.onSupportNavigateUp()
+    }
+
+}
